3f2c47d273319b7c2cec325f4d1f862e38bd802f
Jamil <gafforovabdujamil@gmail.com>
2025-04-14 15:41:18 +0500

    upgrade

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,13 +24,8 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: RootLayoutProps) {
     return (
         <html lang="ru">
-        <head>
-            <meta charSet="utf-8"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <title>FAQ</title>
-        </head>
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
-            {children}
+        {children}
         </body>
         </html>
     );
