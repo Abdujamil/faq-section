8b7bb60670380492efd376bd2d5e6ccb3f708c6e
Jamil <gafforovabdujamil@gmail.com>
2025-04-15 12:54:27 +0500

    change size img

--- src/Components/FaqCard.tsx
+++ src/Components/FaqCard.tsx
@@ -72,7 +72,7 @@ const FaqCard: React.FC<FaqCardProps> = ({id, num, question, answer, src, defaul
                     {/*/>*/}
 
                     <div
-                        className={`${styles.logoOnHover} absolute right-[8%] overflow-hidden opacity-0 z-[9999] w-[173px] h-[153px] border border-[#CCCCCC] backdrop-blur-[2.5px]  rounded-[4px] transition-all ease-in-out duration-[0.3s]`}
+                        className={`${styles.logoOnHover} absolute right-[8%] overflow-hidden opacity-0 z-[9999] w-[155px] h-[155px] border border-[#CCCCCC] backdrop-blur-[2.5px]  rounded-[4px] transition-all ease-in-out duration-[0.3s]`}
                         style={{
                             display: isOpen ? "none" : "block",
                         }}
@@ -80,9 +80,13 @@ const FaqCard: React.FC<FaqCardProps> = ({id, num, question, answer, src, defaul
                         <Image
                             src={src}
                             className="rounded-[4px] opacity-[80%]"
+                            style={{
+                                objectFit: 'contain', // или 'cover' в зависимости от потребностей
+                                aspectRatio: '1 / 1' // явное указание соотношения
+                            }}
                             alt="FAQ image"
-                            width={173}
-                            height={153}
+                            width={155}
+                            height={155}
                         />
                     </div>
 
