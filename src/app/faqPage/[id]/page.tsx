288eeaeca68b458787561ebf3542ca2c9e154e1a
Jamil <gafforovabdujamil@gmail.com>
2025-04-20 20:58:42 +0500

    refactoring [id]

--- src/app/faqPage/[id]/page.tsx
+++ src/app/faqPage/[id]/page.tsx
@@ -5,13 +5,11 @@ import Link from "next/link";
 import styles from "../../page.module.scss";
 import CardListt from "../../../сomponents/FaqPageCard/ShowCardList";
 
-interface Props {
-    params: {
-        id: string;
-    };
-}
-
-export default async function FaqPage({params}: Props) {
+export default async function FaqPage({
+                                          params,
+                                      }: {
+    params: { id: string };
+}) {
     const id = parseInt(params.id);
     const faqItem = faqData.find((item) => item.id === id);
 
@@ -21,25 +19,43 @@ export default async function FaqPage({params}: Props) {
         <div className="w-full max-w-[1160px] mx-auto pt-[80px] pr-[10px] pl-[10px] flex gap-[58px]">
             {/* Sidebar */}
             <aside className="w-1/4 sticky top-20">
-                <Image src={faqItem.src} alt={faqItem.question} width={323} height={323}
-                       className="rounded-lg mb-6 border border-[#3D9ED6]"/>
+                <Image
+                    src={faqItem.src}
+                    alt={faqItem.question}
+                    width={323}
+                    height={323}
+                    className="rounded-lg mb-6 border border-[#3D9ED6]"
+                />
 
                 <Link href={`/`} className="group">
                     <button
                         className="group-hover:border-[#3D9ED6] transition-all duration-[.3s] ease-in flex justify-between w-full cursor-pointer font-bold text-left items-center mb-5 py-3 border-b border-[#CCCCCC]">
                         Краткое содержание
-
-                        <svg width="10" height="11" viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M7.91602 8.41797L2.08268 2.58464" stroke="#CCCCCC" strokeLinecap="round"
-                                  strokeLinejoin="round"/>
-                            <path d="M2.08268 5.91797L2.08268 2.58464L5.41602 2.58464" stroke="#CCCCCC"
-                                  strokeLinecap="round" strokeLinejoin="round"/>
+                        <svg
+                            width="10"
+                            height="11"
+                            viewBox="0 0 10 11"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M7.91602 8.41797L2.08268 2.58464"
+                                stroke="#CCCCCC"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            />
+                            <path
+                                d="M2.08268 5.91797L2.08268 2.58464L5.41602 2.58464"
+                                stroke="#CCCCCC"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            />
                         </svg>
                     </button>
                 </Link>
 
                 <ul className="space-y-4 text-[#737373] font-bold text-sm">
-                    {faqItem.aside?.map(item => (
+                    {faqItem.aside?.map((item) => (
                         <li key={item.id}>
                             <a href={item.id}>{item.title}</a>
                         </li>
@@ -49,8 +65,10 @@ export default async function FaqPage({params}: Props) {
 
             {/* Main content */}
             <main className="w-3/4">
-                <section className={`${styles.accordion} w-full flex flex-col gap-[5px]`}>
-                    <CardListt />
+                <section
+                    className={`${styles.accordion} w-full flex flex-col gap-[5px]`}
+                >
+                    <CardListt/>
                 </section>
             </main>
         </div>
